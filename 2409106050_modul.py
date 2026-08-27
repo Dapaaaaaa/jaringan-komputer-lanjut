99aada8c0907c77e3623bdfa1cab8de3d3001086
Dapaa <daffaharahap191205@gmail.com>
2026-08-28 00:22:24 +0800

    feat: initial setup

---
+++ 2409106050_modul.py
@@ -0,0 +1,8 @@
+# Ndik tahu yg mna
+# Ndik tahu jua
+
+'''
+Nama File : 2409106050_modul.py
+Tujuan    : Program ini adalah module untuk network automation pembuatan ID untuk perangkat pada cabang kantor virtual
+Nama pembuat : Ananda Daffa Harahap 
+'''
---
+++ 2409106050_utama.py
@@ -0,0 +1,8 @@
+# Ndik tahu yg mna
+# Ndik tahu jua
+
+'''
+Nama File : 2409106050_utama.py
+Tujuan    : Program ini adalah program utama untuk menjalankan manajemen jaringan kantor cabang virtual
+Nama pembuat : Ananda Daffa Harahap 
+'''
