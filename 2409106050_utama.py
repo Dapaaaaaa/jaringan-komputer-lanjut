7577ad87504f2c46c17f6a76b432e90fec16b234
Dapaa <daffaharahap191205@gmail.com>
2026-08-28 21:02:13 +0800

    feat: adding section A for identity and make id for device

--- 2409106050_utama.py
+++ 2409106050_utama.py
@@ -6,6 +6,11 @@ Nama File : 2409106050_utama.py
 Tujuan    : Program ini adalah program utama untuk menjalankan manajemen jaringan kantor cabang virtual
 Nama pembuat : Ananda Daffa Harahap 
 '''
+# Membuat modul sesuai perintah soal A
+def buat_id_perangkat(jenis, kode_cabang, nomor):
+    id_perangkat = f"{jenis}-{kode_cabang}-{nomor:02d}"
+    return id_perangkat
+
 # Alasan menggunakan importlib karena modul gabisa di panggil kalau menggunakan nama file awalan angka
 import importlib
 
@@ -23,3 +28,4 @@ kode_cabang = nim[3:]
 id_router = buat_id_perangkat('RTR', kode_cabang, 1)
 id_switch = buat_id_perangkat('SW', kode_cabang, 1)
 id_firewall = buat_id_perangkat('FW', kode_cabang, 1)
+
