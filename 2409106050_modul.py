91157ddf98c1edca49d52df422f8c1b9365bd4fb
Dapaa <daffaharahap191205@gmail.com>
2026-08-28 20:56:18 +0800

    feat: using importlib for import module with numeric name

--- 2409106050_modul.py
+++ 2409106050_modul.py
@@ -1,8 +1,12 @@
-# Ndik tahu yg mna
-# Ndik tahu jua
+# !/usr/bin/env python3
+# Decoder : UTF-8
 
 '''
 Nama File : 2409106050_modul.py
 Tujuan    : Program ini adalah module untuk network automation pembuatan ID untuk perangkat pada cabang kantor virtual
 Nama pembuat : Ananda Daffa Harahap 
 '''
+
+def buat_id_perangkat(jenis, kode_cabang, nomor):
+    id_perangkat = f"{jenis}-{kode_cabang}-{nomor:02d}"
+    return id_perangkat
--- 2409106050_utama.py
+++ 2409106050_utama.py
@@ -1,8 +1,16 @@
-# Ndik tahu yg mna
-# Ndik tahu jua
+# !/usr/bin/env python3
+# Decoder : UTF-8
 
 '''
 Nama File : 2409106050_utama.py
 Tujuan    : Program ini adalah program utama untuk menjalankan manajemen jaringan kantor cabang virtual
 Nama pembuat : Ananda Daffa Harahap 
 '''
+# Alasan menggunakan importlib karena modul gabisa di panggil kalau menggunakan nama file awalan angka
+import importlib
+
+# Kita buat variabel untuk memanggil modul dari file 2409106050_modul.py
+modul = importlib.import_module('2409106050_modul')
+# ini adalah contoh pemanggilan fungsi untuk buat_id_perangkat dari modul
+buat_id_perangkat = modul.buat_id_perangkat
+
